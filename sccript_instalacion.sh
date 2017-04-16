ffd27e3598e1f597c2216017db00a5f061142bcb
ChristianBarrientos <christianbarrientoss@hotmail.com>
2017-04-17 00:34:43 +0200

    Agregando controles

--- sccript_instalacion.sh
+++ sccript_instalacion.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
 apt-get install proftpd -y
+if [ "$?" != "0" ]; then echo "Error instlacion"; exit 1; fi
+cp /proftpd.conf /etc/proftpd/
+if [ "$?" != "0" ]; then echo "Copiadno Archivo de configuracion "; exit 1; fi
 addgroup ftp
+if [ "$?" != "0" ]; then echo "Creando Grupo de trabajo"; exit 1; fi
 adduser ftp ftp
+if [ "$?" != "0" ]; then echo "Creando usuario de trabajo y agregando a grupo"; exit 1; fi
 mkdir /home/ftp
+if [ "$?" != "0" ]; then echo "Creando directorio ftp"; exit 1; fi
 mkdir /home/ftp/up
+if [ "$?" != "0" ]; then echo "Creando directorio de subida"; exit 1; fi
 mkdir /home/ftp/down
+if [ "$?" != "0" ]; then echo "Creadno directorio de bajada"; exit 1; fi
 cd /home/ftp
+if [ "$?" != "0" ]; then echo "Moviendoce a carpeta de trabajo"; exit 1; fi
 chown ftp:ftp down
+if [ "$?" != "0" ]; then echo "Haciendo propietario directorio bajada"; exit 1; fi
 chown ftp:ftp up
+if [ "$?" != "0" ]; then echo "Haciendo propietario directorio subida"; exit 1; fi
